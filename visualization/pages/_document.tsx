331911bc89964de8d6932fcba7824f92a3268492
Dhairya Gupta <dhairyag23@gmail.com>
2024-03-30 15:11:17 -0500

    upda

--- visualization/pages/_document.tsx
+++ visualization/pages/_document.tsx
@@ -3,8 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <title>DataHack '24 Results</title>
+      </Head>
+      <body className="prose max-w-lg p-x-auto">
         <Main />
         <NextScript />
       </body>
--- visualization/pages/index.tsx
+++ visualization/pages/index.tsx
@@ -1,15 +1,16 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Barchart from "../components/Barchart";
+import * as Papa from "papaparse";
+
+import { useCSVReader } from "react-papaparse";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <Barchart />
-    </main>
-  );
+  const data = Papa.parse("data/batting_season_summary.csv");
+  const { CSVReader } = useCSVReader();
+
+  console.log(data);
+  return <></>;
 }
